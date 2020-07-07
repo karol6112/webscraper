aa28358cabd6f563eca6e33e57bc809b5f91d2b2
Karol <karol.kolerzak@gmail.com>
2020-07-07 16:31:26 +0200

    add raise exception

--- web_scraper.py
+++ web_scraper.py
@@ -1,6 +1,10 @@
-import requests, time, psycopg2, smtplib
-import config
 from bs4 import BeautifulSoup
+import requests
+import time
+import psycopg2
+import smtplib
+
+import config
 
 TABLE_NAME = "PRICE_ALERT"
 
@@ -37,49 +41,47 @@ def send_email(url, price):
         server.sendmail(user, to, message)
         server.quit()
         print("Alert has been sent")
-    except:
-        print("ERROR. CAN'T SEND ALERT")
+    except Exception as error:
+        print("ERROR. CAN'T SEND ALERT", error)
 
 
 def execute(query):
     try:
         connection = psycopg2.connect(config.DATA_TO_CONNECT)
-    except (Exception, psycopg2.Error) as error:
-        print("Connecting Error", error)
-        connection = None
-
-    cursor = connection.cursor()
-
-    try:
+        cursor = connection.cursor()
         cursor.execute(query)
     except (Exception, psycopg2.Error) as error:
-        print("Execute query Error", error)
-        connection = None
-
-    finally:
+        print("Error:", error)
+    else:
         connection.commit()
-        if connection != None:
-            cursor.close()
-            connection.close()
+        cursor.close()
+        connection.close()
 
 
 def find_id(response):
-    soup = BeautifulSoup(response.text, 'html.parser')
-    ad_id = int(soup.find(id="ad_id").text)
-    return ad_id
+    try:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        ad_id = int(soup.find(id="ad_id").text)
+    except Exception as error:
+        raise error
+    else:
+        return ad_id
 
 
 def find_price(response):
-    soup = BeautifulSoup(response.text, 'html.parser')
-    price = soup.find(class_="offer-price__number")
-    price = str(price.contents[0])
-    price = price.split(" ")
-    price = int(price[0] + price[1])
-    return price
+    try:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        price = soup.find(class_="offer-price__number")
+        price = str(price.contents[0])
+        price = price.split(" ")
+        price = int(price[0] + price[1])
+    except Exception as error:
+        raise error
+    else:
+        return price
 
 
 if __name__ == "__main__":
-
     execute(CREATE_QUERY)
     urls = read_urls()
 
@@ -93,9 +95,9 @@ if __name__ == "__main__":
                     price = find_price(response)
 
                     insert_query = f""" INSERT INTO {TABLE_NAME} VALUES (default, default, {ad_id}, {price})"""
-                    print(price)
-
                     execute(insert_query)
+
+                    print(price)
 
                     if price <= price_alert:
                         send_email(url, price)
@@ -106,11 +108,11 @@ if __name__ == "__main__":
                     print("The advertisement is out of date")
                 else:
                     print('Connection to the website failed')
-            except:
-                print('Error')
+            except Exception as error:
+                print('Error:', error)
 
         if not urls:
             print("Urls.txt has not active links. Add links to ad")
             break
 
-        time.sleep(1)  # time in second between downloading data (86400 = 24h)
+        time.sleep(10)  # time in second between downloading data (86400 = 24h)
